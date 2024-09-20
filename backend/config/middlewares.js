479cd804f4007816f64b3862de789c13db1c2d3d
Pedro Poveda <pedropovedaq@gmail.com>
2024-09-19 21:04:51 -0400

    add cors

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -3,12 +3,12 @@ module.exports = [
   'strapi::security',
   {
     name: 'strapi::cors',
-    config: {
-      origin: ['https://example.com', 'https://subdomain.example.com', 'https://someotherwebsite.org'],
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
-      keepHeaderOnError: true,
-    },
+    // config: {
+    //   origin: ['https://example.com', 'https://subdomain.example.com', 'https://someotherwebsite.org'],
+    //   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+    //   headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+    //   keepHeaderOnError: true,
+    // },
   },
   'strapi::poweredBy',
   'strapi::logger',
